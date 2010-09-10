e4b52148c326f34e8e01e0acbc78a79bfb527855
hamilr2 <hamilr2@rpi.edu>
2010-09-09 20:37:31 -0400

    Removing some extraneous files that were accidentally included in initial commit.

--- dir.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-  $handle = opendir('.');
-  while (false !== ($file = readdir($handle))) {
-    if(is_dir($file))
-      echo("<a href=\"gallery.php?dir=$file&w=400\"/>$file</a><br>\n\n");
-  }
-?>
