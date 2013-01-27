ae4814332aea0e0b3b11416e1eef1bc654c6bf5b
Reilly Hamilton <reilly.hamilton@espn.com>
2013-01-26 22:33:16 -0800

    Missed this in a prior commit

--- im_ui.js
+++ im_ui.js
@@ -1,3 +1,5 @@
+var ui = {};
+
 $.extend($.expr[":"], {
 	"containsC": function(elem, i, match, array) {
 		return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
