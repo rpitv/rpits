a0fcadf8785983a69119ed7cc1f4227d7bda1917
Reilly Hamilton <reilly.hamilton@espn.com>
2013-02-01 00:31:08 -0800

    If new line characters are detected, create multi-line textareas

--- im_edit_title.php
+++ im_edit_title.php
@@ -1,82 +1,73 @@
 <link rel="stylesheet" type="text/css" href="im_edit.css" media="screen" />
-<!--<script src="js/jquery-1.5.1.min.js" type="text/javascript"></script>
-<script src="js/jquery-ui-1.8.12.custom.min.js" type="text/javascript"></script>-->
+<script src="js/jquery-1.5.1.min.js" type="text/javascript"></script>
+<script src="js/jquery-ui-1.8.12.custom.min.js" type="text/javascript"></script>
 <?php
 
 include("include.php");
 
-$title_id = $_GET["id"];
+$titleId = $_GET["id"];
 
-$result = dbquery("SELECT * from titles where id=\"$title_id\" LIMIT 1;");
-$titleRow = mysql_fetch_array($result);
+$titleResult = dbquery("SELECT * from titles where id=\"$titleId\" LIMIT 1;");
+$titleRow = mysql_fetch_array($titleResult);
 
-$template_id = $titleRow["template"];
+$templateId = $titleRow["template"];
 
-$result = dbquery("SELECT * from templates where id=\"$template_id\" LIMIT 1;");
-$templateRow = mysql_fetch_array($result);
+$templateResult = dbquery("SELECT * from templates where id=\"$templateId\" LIMIT 1;");
+$templateRow = mysql_fetch_array($templateResult);
 
 $templateXML = fopen($templateRow["path"],"r");
 $contents = stream_get_contents($templateXML);
 
-$xml = new SimpleXMLElement($contents);
+function printEditableRow($name,$id,$val) {
+	$val = str_replace('\n',PHP_EOL,$val);
+	$newlines = substr_count($val,PHP_EOL);
+	echo '<div class="row">';
+  echo '<div class="label">'.$name.'</div>';
+  echo '<div class="form"><form class="edit_form" action="javascript:true" method="GET">';
+  echo '<input type="hidden" name="' . $id . '" value="' . $name .'" />';
+	if($newlines > 0) {
+		echo '<textarea rows="' . ($newlines+1) . '" name="text">' . "\n" . $val . '</textarea>';
+	} else {
+		echo '<input type="text" name="text" value="' . $val . '" />';
+	}
+  echo '<input class="submit" type="submit" value="Update" />';
+  echo '</form></div>';
+	echo '</div>';
+}
 
-$lc = "<div id=\"left-column\">\n";
-$rc = "<div id=\"right-column\">\n";
-$lc .="<h3>Shadow Text</h3>\n";
-$rc .="<h3>&nbsp</h3>\n";
+$xml = new SimpleXMLElement($contents);
+echo '<div id="editTitle">';
+echo "<h3>Shadow Text</h3>";
 foreach($xml->overlay->shadowText as $text)
 {
-  $t = dbFetch($title_id,$text);
-  $lc .= "<p>".$t["name"]."</p>\n";
-  $rc .= "<form class=\"edit_form\" action\"javascript:true\" \"method=\"GET\">";
-  $rc .= "\t<input type=\"hidden\" name=\"" . $title_id . "\" value=\"" . $t["name"] . "\" />";
-  $rc .= "\t<input type=\"text\" name=\"text\" value=\"" . $t["text"] . "\" />\n";
-  $rc .= "\t<input class=\"submit\" type=\"submit\" value=\"Update\" />";
-  $rc .= "</form>\n";
+  $t = dbFetch($titleId,$text);
+	printEditableRow($t["name"],$titleId,$t["text"]);
 }
-$lc .="<h3>Normal Text</h3>\n";
-$rc .="<h3>&nbsp</h3>\n";
+echo "<h3>Normal Text</h3>";
 foreach($xml->overlay->plainText as $text)
 {
-  $t = dbFetch($title_id,$text);
-  $lc .= "<p>".$t["name"]."</p>\n";
-  $rc .= "<form class=\"edit_form\" action\"javascript:true\" \"method=\"GET\">";
-  $rc .= "\t<input type=\"hidden\" name=\"" . $title_id . "\" value=\"" . $t["name"] . "\" />";
-  $rc .= "\t<input type=\"text\" name=\"text\" value=\"" . $t["text"] . "\" />\n";
-  $rc .= "\t<input class=\"submit\" type=\"submit\" value=\"Update\" />";
-  $rc .= "</form>\n";
+  $t = dbFetch($titleId,$text);
+  printEditableRow($t["name"],$titleId,$t["text"]);
 }
-$lc .="<h3>Color Bars</h3>\n";
-$rc .="<h3>&nbsp</h3>\n";
+echo "<h3>Color Bars</h3>";
+
 foreach($xml->geo->slantRectangle as $slantRectangle)
 {
-  $t = dbFetch($title_id,$slantRectangle);
-  $lc .= "<p>".$t["name"]."</p>\n";
-  $rc .= "<form class=\"edit_form\" action\"javascript:true\" \"method=\"GET\">";
-  $rc .= "\t<input type=\"hidden\" name=\"" . $title_id . "\" value=\"" . $t["name"] . "\" />";
-  $rc .= "\t<input type=\"text\" name=\"color\" value=\"" . $t["color"] . "\" />\n";
-  $rc .= "\t<input class=\"submit\" type=\"submit\" value=\"Update\" />";
-  $rc .= "</form>\n";
+  $t = dbFetch($titleId,$slantRectangle);
+  printEditableRow($t["name"],$titleId,$t["color"]);
 }
-$lc .="<h3>Images</h3>\n";
-$rc .="<h3>&nbsp</h3>\n";
+echo "<h3>Images</h3>";
+
 foreach($xml->overlay->placeImage as $image)
 {
-  $t = dbFetch($title_id,$image);
-  $lc .= "<p>".$t["name"]."</p>\n";
-  $rc .= "<form class=\"edit_form\" action\"javascript:true\" \"method=\"GET\">";
-  $rc .= "\t<input type=\"hidden\" name=\"" . $title_id . "\" value=\"" . $t["name"] . "\" />";
-  $rc .= "\t<input type=\"text\" name=\"path\" value=\"" . $t["path"] . "\" />\n";
-  $rc .= "\t<input class=\"submit\" type=\"submit\" value=\"Update\" />";
-  $rc .= "</form>\n";
+  $t = dbFetch($titleId,$image);
+  printEditableRow($t["name"],$titleId,$t["path"]);
 }
 
-$lc .= "</div>\n";
-$rc .= "</div>\n";
-
-echo $lc . $rc;
+echo '</div>'
+	
 ?><br style="clear:both" />
-<button tid="<?= $title_id ?>" id="render" name="Render" style="width:200px;margin-bottom:70px;margin-top:70px;" >Render</button>
+<button tid="<?= $titleId ?>" id="render" name="Render" style="width:200px;margin-bottom:70px;margin-top:70px;" >Render</button>
 <script type="text/javascript">
 $(".edit_form").submit(function() {
   var form = $(this);
