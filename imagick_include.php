2e45c84295547eef10f467d84c51c3f45667a223
Reilly Hamilton <reilly.hamilton@espn.com>
2013-03-04 21:14:08 -0800

    Code formatting adjustments; Allman -> K&R mostly

--- imagick_include.php
+++ imagick_include.php
@@ -1,232 +1,221 @@
 <?
 
-$gravities = array("west"=>imagick::GRAVITY_WEST,"center"=>imagick::GRAVITY_CENTER,"east"=>imagick::GRAVITY_EAST);
-$fonts = array("fontN"=>"fonts/GothamNarrow-Bold.otf","font"=>"fonts/Gotham-Bold.ttf","fontX"=>"fonts/GothamXNarrow-Bold.otf");
+$gravities = array("west" => imagick::GRAVITY_WEST, "center" => imagick::GRAVITY_CENTER, "east" => imagick::GRAVITY_EAST);
+$fonts = array("fontN" => "fonts/GothamNarrow-Bold.otf", "font" => "fonts/Gotham-Bold.ttf", "fontX" => "fonts/GothamXNarrow-Bold.otf");
 
-function slantRectangle(&$canvas,$x,$y,$w,$h,$color1,$image=0)
-{
-  $background = "#FFFFFF";
-  //$ltr = '#000000';
-  $ltr='#888';
-  $bd = '#A6A6A6';
-  $iglow='#404040';
+function slantRectangle(&$canvas, $x, $y, $w, $h, $color1, $image = 0) {
+	$background = "#FFFFFF";
+	//$ltr = '#000000';
+	$ltr = '#888';
+	$bd = '#A6A6A6';
+	$iglow = '#404040';
 
-  try{
-    
-    $gradient1 = new Imagick();
-    $gradient1->newPseudoImage($h, $w*3/8, "gradient:white-$ltr");
-    $gradient1->rotateImage(new ImagickPixel(),270);
-    $gradient2 = new Imagick();
-    $gradient2->newPseudoImage($h, $w*3/8, "gradient:$ltr-white");
-    $gradient2->rotateImage(new ImagickPixel(),270);
+	try {
 
-    $lefttoright = new Imagick();
-    $lefttoright->newPseudoImage($w,$h,"xc:$ltr");
-    $lefttoright->compositeImage($gradient1,imagick::COMPOSITE_OVER,0,0);
-    $lefttoright->compositeImage($gradient2,imagick::COMPOSITE_OVER,$w*5/8,0);
+		$gradient1 = new Imagick();
+		$gradient1->newPseudoImage($h, $w * 3 / 8, "gradient:white-$ltr");
+		$gradient1->rotateImage(new ImagickPixel(), 270);
+		$gradient2 = new Imagick();
+		$gradient2->newPseudoImage($h, $w * 3 / 8, "gradient:$ltr-white");
+		$gradient2->rotateImage(new ImagickPixel(), 270);
 
-    $whiteup = new Imagick();
-    $whiteup->newPseudoImage($w,$h/2,"gradient:black-#888");
+		$lefttoright = new Imagick();
+		$lefttoright->newPseudoImage($w, $h, "xc:$ltr");
+		$lefttoright->compositeImage($gradient1, imagick::COMPOSITE_OVER, 0, 0);
+		$lefttoright->compositeImage($gradient2, imagick::COMPOSITE_OVER, $w * 5 / 8, 0);
 
-    $gradient1 = new Imagick();
-    $gradient1-> newPseudoImage($w,$h/7,"gradient:$bd-white");
-    $gradient2 = new Imagick();
-    $gradient2-> newPseudoImage($w,$h/7,"gradient:white-$bd");
+		$whiteup = new Imagick();
+		$whiteup->newPseudoImage($w, $h / 2, "gradient:black-#888");
 
-    $bottomdark = new Imagick();
-    $bottomdark->newPseudoImage($w,$h,"xc:white");
-    $bottomdark->compositeImage($gradient1,imagick::COMPOSITE_OVER,0,0);
-    $bottomdark->compositeImage($gradient2,imagick::COMPOSITE_OVER,0,($h/2)-($h/7));
+		$gradient1 = new Imagick();
+		$gradient1->newPseudoImage($w, $h / 7, "gradient:$bd-white");
+		$gradient2 = new Imagick();
+		$gradient2->newPseudoImage($w, $h / 7, "gradient:white-$bd");
 
-    $background = new Imagick();
-    $background->newPseudoImage($w,$h,"xc:$color1");
-    
-    
+		$bottomdark = new Imagick();
+		$bottomdark->newPseudoImage($w, $h, "xc:white");
+		$bottomdark->compositeImage($gradient1, imagick::COMPOSITE_OVER, 0, 0);
+		$bottomdark->compositeImage($gradient2, imagick::COMPOSITE_OVER, 0, ($h / 2) - ($h / 7));
 
-    
-    $background->compositeImage($lefttoright,imagick::COMPOSITE_MULTIPLY,0,0);
-    if($image)
-    {
-      $logo = new Imagick();
-      $logo->readImage(realpath($image));
-      $logo->resizeImage($h,$h,imagick::FILTER_TRIANGLE,1);
-      $background->compositeImage($logo,imagick::COMPOSITE_OVER,$w-$h-($h/3),0);
-    }
-    $background->compositeImage($whiteup,imagick::COMPOSITE_SCREEN,0,0);
-    $background->compositeImage($bottomdark,imagick::COMPOSITE_MULTIPLY,0,$h/2);
+		$background = new Imagick();
+		$background->newPseudoImage($w, $h, "xc:$color1");
 
-    $slantleft = new Imagick();
-    $slantleft->newPseudoImage($h*sqrt(5)/2,8,"gradient:$iglow-white");
-    $slantleft->rotateImage("none",296.6);
-    $slantright = new Imagick();
-    $slantright->newPseudoImage($h*sqrt(5)/2,8,"gradient:$iglow-white");
-    $slantright->rotateImage("none",117.2);
 
-    $top = new Imagick();
-    $top->newPseudoImage($w,8,"gradient:$iglow-white");
-    $bottom = new Imagick();
-    $bottom->newPseudoImage($w,8,"gradient:white-$iglow");
 
-    $slants = new Imagick();
-    $slants->newPseudoImage($w,$h,"xc:white");
-    $slants->compositeImage($slantleft,imagick::COMPOSITE_OVER,-1,0);
-    $slants->compositeImage($slantright,imagick::COMPOSITE_OVER,$w-($h/2)-9,0);
-    $slants->compositeImage($top,imagick::COMPOSITE_MULTIPLY,0,0);
-    $slants->compositeImage($bottom,imagick::COMPOSITE_MULTIPLY,0,$h-8);
 
-    $background->compositeImage($slants,imagick::COMPOSITE_MULTIPLY,0,00);
-    
-    $draw1 = new ImagickDraw();
-    $draw1->pushPattern('gradient',0,0,$w,$h);
-    $draw1->composite(Imagick::COMPOSITE_OVER,0,0,$w,$h,$background);
-    $draw1->popPattern();
-    $draw1->setFillPatternURL('#gradient');
-    $draw1->polygon(array(array('x'=>00,'y'=>$h-1), array('x'=>($h/2)-1, 'y'=>00), array('x'=>$w-1,'y'=> 00),array('x'=>$w-($h/2)-1,'y'=>$h-1)));      
+		$background->compositeImage($lefttoright, imagick::COMPOSITE_MULTIPLY, 0, 0);
+		if ($image) {
+			$logo = new Imagick();
+			$logo->readImage(realpath($image));
+			$logo->resizeImage($h, $h, imagick::FILTER_TRIANGLE, 1);
+			$background->compositeImage($logo, imagick::COMPOSITE_OVER, $w - $h - ($h / 3), 0);
+		}
+		$background->compositeImage($whiteup, imagick::COMPOSITE_SCREEN, 0, 0);
+		$background->compositeImage($bottomdark, imagick::COMPOSITE_MULTIPLY, 0, $h / 2);
 
-    $points = array(array('x'=>0,'y'=>$h-1), array('x'=>($h/2)-1, 'y'=>00), array('x'=>$w-1,'y'=> 00),array('x'=>$w-($h/2)-1,'y'=>$h-1));
+		$slantleft = new Imagick();
+		$slantleft->newPseudoImage($h * sqrt(5) / 2, 8, "gradient:$iglow-white");
+		$slantleft->rotateImage("none", 296.6);
+		$slantright = new Imagick();
+		$slantright->newPseudoImage($h * sqrt(5) / 2, 8, "gradient:$iglow-white");
+		$slantright->rotateImage("none", 117.2);
 
-    for($i=0;$i<4;$i++)
-    {
-      $points[$i]['x']+=10;
-      $points[$i]['y']+=10;
-    }
-    
-    $shadow = new Imagick();
-    $shadow->newPseudoImage($w+20,$h+20,"xc:none");
-    $draws = new ImagickDraw();
-    $draws->setFillColor("black");
-    $draws->polygon($points);
-    $shadow->drawImage($draws);
-    $shadow->blurImage(0,4,imagick::CHANNEL_ALPHA);
-    
-    $im = new Imagick();
-    $im->newPseudoImage( $w, $h, "xc:none");
+		$top = new Imagick();
+		$top->newPseudoImage($w, 8, "gradient:$iglow-white");
+		$bottom = new Imagick();
+		$bottom->newPseudoImage($w, 8, "gradient:white-$iglow");
 
-    $im->drawImage($draw1);
+		$slants = new Imagick();
+		$slants->newPseudoImage($w, $h, "xc:white");
+		$slants->compositeImage($slantleft, imagick::COMPOSITE_OVER, -1, 0);
+		$slants->compositeImage($slantright, imagick::COMPOSITE_OVER, $w - ($h / 2) - 9, 0);
+		$slants->compositeImage($top, imagick::COMPOSITE_MULTIPLY, 0, 0);
+		$slants->compositeImage($bottom, imagick::COMPOSITE_MULTIPLY, 0, $h - 8);
 
-    $im2 = new Imagick();
-    $im2->newPseudoImage($w+50,$h+50,"xc:none");
-    $im2->compositeImage($shadow,imagick::COMPOSITE_OVER,5,5);
-    $draw1 = new ImagickDraw();
-    $draw1->setStrokeWidth(6);
-    $draw1->setStrokeColor("black");
+		$background->compositeImage($slants, imagick::COMPOSITE_MULTIPLY, 0, 00);
 
-    $draw1->polygon($points);  
-    $draw1->setStrokeWidth(2);
-    $draw1->setStrokeColor("white");
-    $draw1->polygon($points);  
-    $im2->drawImage($draw1);
-    
-    $im2->compositeImage($im,imagick::COMPOSITE_OVER,10,10);
-    
-    
+		$draw1 = new ImagickDraw();
+		$draw1->pushPattern('gradient', 0, 0, $w, $h);
+		$draw1->composite(Imagick::COMPOSITE_OVER, 0, 0, $w, $h, $background);
+		$draw1->popPattern();
+		$draw1->setFillPatternURL('#gradient');
+		$draw1->polygon(array(array('x' => 00, 'y' => $h - 1), array('x' => ($h / 2) - 1, 'y' => 00), array('x' => $w - 1, 'y' => 00), array('x' => $w - ($h / 2) - 1, 'y' => $h - 1)));
 
-    $canvas->compositeImage($im2,imagick::COMPOSITE_OVER,$x-10,$y-10);
+		$points = array(array('x' => 0, 'y' => $h - 1), array('x' => ($h / 2) - 1, 'y' => 00), array('x' => $w - 1, 'y' => 00), array('x' => $w - ($h / 2) - 1, 'y' => $h - 1));
 
-  }catch(Exception $e){
-    echo 'Error: ',  $e->getMessage(), "";
-  }
+		for ($i = 0; $i < 4; $i++) {
+			$points[$i]['x']+=10;
+			$points[$i]['y']+=10;
+		}
+
+		$shadow = new Imagick();
+		$shadow->newPseudoImage($w + 20, $h + 20, "xc:none");
+		$draws = new ImagickDraw();
+		$draws->setFillColor("black");
+		$draws->polygon($points);
+		$shadow->drawImage($draws);
+		$shadow->blurImage(0, 4, imagick::CHANNEL_ALPHA);
+
+		$im = new Imagick();
+		$im->newPseudoImage($w, $h, "xc:none");
+
+		$im->drawImage($draw1);
+
+		$im2 = new Imagick();
+		$im2->newPseudoImage($w + 50, $h + 50, "xc:none");
+		$im2->compositeImage($shadow, imagick::COMPOSITE_OVER, 5, 5);
+		$draw1 = new ImagickDraw();
+		$draw1->setStrokeWidth(6);
+		$draw1->setStrokeColor("black");
+
+		$draw1->polygon($points);
+		$draw1->setStrokeWidth(2);
+		$draw1->setStrokeColor("white");
+		$draw1->polygon($points);
+		$im2->drawImage($draw1);
+
+		$im2->compositeImage($im, imagick::COMPOSITE_OVER, 10, 10);
+
+
+
+		$canvas->compositeImage($im2, imagick::COMPOSITE_OVER, $x - 10, $y - 10);
+	} catch (Exception $e) {
+		echo 'Error: ', $e->getMessage(), "";
+	}
 }
 
-function blackBox(&$canvas,$x,$y,$w,$h)
-{
-  $rectangle = new Imagick();
-  $rectangle->newPseudoImage($w,$h,"xc:none");
-  $draw1 = new ImagickDraw();
-  $draw1->pushPattern('gradient',0,0,5,5);
-  $tile = new Imagick();
-  $tile->readImage(realpath("assets/diag_tile.png"));
-  $draw1->composite(Imagick::COMPOSITE_OVER,0,0,5,5,$tile);
-  $draw1->popPattern();
-  $draw1->setFillPatternURL('#gradient');
-  $draw1->rectangle(0,0,$w,$h);
-  $rectangle->drawImage($draw1);
-  
-  $gradient = new Imagick();
-  $gradient->newPseudoImage($w,$h,"gradient:#DDD-#666");
-  
-  $rectangle->compositeImage($gradient,Imagick::COMPOSITE_COPYOPACITY,0,0);
-  
-  $black = new Imagick();
-  $black->newPseudoImage($w,$h,"xc:black");
-  
-  $layered = new Imagick();
-  $layered->newPseudoImage($w+20,$h+20,"xc:none");
-  $layered->compositeImage($black,Imagick::COMPOSITE_OVER,5,0);
-  $layered->compositeImage($black,Imagick::COMPOSITE_OVER,5,5);
-  $layered->compositeImage($gradient,Imagick::COMPOSITE_COPYOPACITY,5,5);
-  $layered->blurImage(4,5,imagick::CHANNEL_ALPHA);
-  $layered->compositeImage($black,Imagick::COMPOSITE_DSTOUT,0,0);
-  
-  $canvas->compositeImage($layered,Imagick::COMPOSITE_OVER,$x,$y);
-  $canvas->compositeImage($rectangle,Imagick::COMPOSITE_OVER,$x,$y);
-  
+function blackBox(&$canvas, $x, $y, $w, $h) {
+	$rectangle = new Imagick();
+	$rectangle->newPseudoImage($w, $h, "xc:none");
+	$draw1 = new ImagickDraw();
+	$draw1->pushPattern('gradient', 0, 0, 5, 5);
+	$tile = new Imagick();
+	$tile->readImage(realpath("assets/diag_tile.png"));
+	$draw1->composite(Imagick::COMPOSITE_OVER, 0, 0, 5, 5, $tile);
+	$draw1->popPattern();
+	$draw1->setFillPatternURL('#gradient');
+	$draw1->rectangle(0, 0, $w, $h);
+	$rectangle->drawImage($draw1);
+
+	$gradient = new Imagick();
+	$gradient->newPseudoImage($w, $h, "gradient:#DDD-#666");
+
+	$rectangle->compositeImage($gradient, Imagick::COMPOSITE_COPYOPACITY, 0, 0);
+
+	$black = new Imagick();
+	$black->newPseudoImage($w, $h, "xc:black");
+
+	$layered = new Imagick();
+	$layered->newPseudoImage($w + 20, $h + 20, "xc:none");
+	$layered->compositeImage($black, Imagick::COMPOSITE_OVER, 5, 0);
+	$layered->compositeImage($black, Imagick::COMPOSITE_OVER, 5, 5);
+	$layered->compositeImage($gradient, Imagick::COMPOSITE_COPYOPACITY, 5, 5);
+	$layered->blurImage(4, 5, imagick::CHANNEL_ALPHA);
+	$layered->compositeImage($black, Imagick::COMPOSITE_DSTOUT, 0, 0);
+
+	$canvas->compositeImage($layered, Imagick::COMPOSITE_OVER, $x, $y);
+	$canvas->compositeImage($rectangle, Imagick::COMPOSITE_OVER, $x, $y);
 }
 
-function defaultText($w,$h,$string,$gravity,$font,$wordWrap=false)
-{
-  global $gravities,$fonts;
-  $text = new Imagick();
-  $text->setFont($fonts[$font]);
-  $text->setBackgroundColor("none");
-  $text->setGravity($gravities[$gravity]);
-  if ($wordWrap) {
-    $text->newPseudoImage($w, $h, "caption:" . $string);
-  } else {
-    $text->newPseudoImage($w,$h,"label:" . $string);
-  }
-  //$metrics = $text->queryFontMetrics( $annotate, $text );
-  
-  return $text;
+function defaultText($w, $h, $string, $gravity, $font, $wordWrap = false) {
+	global $gravities, $fonts;
+	$text = new Imagick();
+	$text->setFont($fonts[$font]);
+	$text->setBackgroundColor("none");
+	$text->setGravity($gravities[$gravity]);
+	if ($wordWrap) {
+		$text->newPseudoImage($w, $h, "caption:" . $string);
+	} else {
+		$text->newPseudoImage($w, $h, "label:" . $string);
+	}
+	//$metrics = $text->queryFontMetrics( $annotate, $text );
+
+	return $text;
 }
 
-function plainText(&$canvas,$x,$y,$w,$h,$string,$gravity,$font,$color,$wordWrap=false)
-{
-  $text = defaultText($w,$h,$string,$gravity,$font,$wordWrap); 
-  $shadow = $text->clone();
-  $shadow->blurImage(4,2,imagick::CHANNEL_ALPHA);
-  $text->colorizeImage($color,1);
-  
-  $canvas->compositeImage($shadow,imagick::COMPOSITE_OVER,$x,$y);
-  $canvas->compositeImage($text,imagick::COMPOSITE_OVER,$x,$y);
+function plainText(&$canvas, $x, $y, $w, $h, $string, $gravity, $font, $color, $wordWrap = false) {
+	$text = defaultText($w, $h, $string, $gravity, $font, $wordWrap);
+	$shadow = $text->clone();
+	$shadow->blurImage(4, 2, imagick::CHANNEL_ALPHA);
+	$text->colorizeImage($color, 1);
+
+	$canvas->compositeImage($shadow, imagick::COMPOSITE_OVER, $x, $y);
+	$canvas->compositeImage($text, imagick::COMPOSITE_OVER, $x, $y);
 }
 
-function getTextWidth($w,$h,$string,$font,$wordWrap=false)
-{
-  $text = defaultText($w,$h,$string,"center",$font,$wordWrap);
-  $text->trimImage(0);
-  $geo = $text->getImageGeometry();
-  return $geo["width"];
+function getTextWidth($w, $h, $string, $font, $wordWrap = false) {
+	$text = defaultText($w, $h, $string, "center", $font, $wordWrap);
+	$text->trimImage(0);
+	$geo = $text->getImageGeometry();
+	return $geo["width"];
 }
 
-function shadowedText(&$canvas,$x,$y,$w,$h,$string,$gravity,$font,$color,$wordWrap=false)
-{
-  $text = defaultText($w,$h,$string,$gravity,$font,$wordWrap);      
-  $shadow = $text->clone();
-  $stroke = $text->clone();
-  $shadow->blurImage(4,5,imagick::CHANNEL_ALPHA);
-  $text->colorizeImage($color,1);
+function shadowedText(&$canvas, $x, $y, $w, $h, $string, $gravity, $font, $color, $wordWrap = false) {
+	$text = defaultText($w, $h, $string, $gravity, $font, $wordWrap);
+	$shadow = $text->clone();
+	$stroke = $text->clone();
+	$shadow->blurImage(4, 5, imagick::CHANNEL_ALPHA);
+	$text->colorizeImage($color, 1);
 
-  $canvas->compositeImage($shadow,imagick::COMPOSITE_OVER,$x+5,$y+5);
-  $canvas->compositeImage($shadow,imagick::COMPOSITE_OVER,$x,$y);
-  $canvas->compositeImage($text,imagick::COMPOSITE_OVER,$x,$y);
+	$canvas->compositeImage($shadow, imagick::COMPOSITE_OVER, $x + 5, $y + 5);
+	$canvas->compositeImage($shadow, imagick::COMPOSITE_OVER, $x, $y);
+	$canvas->compositeImage($text, imagick::COMPOSITE_OVER, $x, $y);
 }
 
-function placeImage(&$canvas,$x,$y,$w,$h,$path)
-{
-  try{
-    $logo = new Imagick();
-    $logo->readImage(realpath($path));
-    
-    // This might cause problems with non-Player Portraits if aspect ratios are off.
-    $size = @getimagesize($path);
-    $logo->cropImage($size[0], $size[0]*1.2, 0, 0);
-    
-    $logo->resizeImage($w,$h,imagick::FILTER_TRIANGLE,1);
-    $canvas->compositeImage($logo,imagick::COMPOSITE_OVER,$x,$y);
-  }
-  catch(Exception $e){
-    echo 'Error: ',  $e->getMessage(), "";
-  }
+function placeImage(&$canvas, $x, $y, $w, $h, $path) {
+	try {
+		$logo = new Imagick();
+		$logo->readImage(realpath($path));
+
+		// This might cause problems with non-Player Portraits if aspect ratios are off.
+		$size = @getimagesize($path);
+		$logo->cropImage($size[0], $size[0] * 1.2, 0, 0);
+
+		$logo->resizeImage($w, $h, imagick::FILTER_TRIANGLE, 1);
+		$canvas->compositeImage($logo, imagick::COMPOSITE_OVER, $x, $y);
+	} catch (Exception $e) {
+		echo 'Error: ', $e->getMessage(), "";
+	}
 }
+
 ?>
--- include.php
+++ include.php
@@ -3,100 +3,91 @@
 include ("init.php");
 mysql_select_db("rpits");
 
-function dbquery($query)
-{
-  $result = mysql_query($query) or die("<b>Error with MySQL Query:</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysql_errno() . ") " . mysql_error());
-  return $result;
-}
-function dbqueryl($query)
-{
-  $result = mysql_query($query);
-  return $result;
+function dbquery($query) {
+	$result = mysql_query($query) or die("<b>Error with MySQL Query:</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysql_errno() . ") " . mysql_error());
+	return $result;
 }
 
-function listOfGeos($id)
-{
-  $geos = array();
-  $title = dbquery("SELECT * from titles left join templates on titles.template=templates.id where titles.id='$id'  ");
-  $result = mysql_fetch_array($title);
-  $templateXML = fopen($result["path"],"r");
-  $contents = stream_get_contents($templateXML);
-  $xml = new SimpleXMLElement($contents);
-  foreach($xml->geo->children() as $geo)
-  {
-    $name = (string) $geo["name"];
-    $geos[$name] = $geo->getName();
-  }
-  foreach($xml->overlay->children() as $geo)
-  {
-    $name = (string) $geo["name"];
-    $geos[$name] = $geo->getName();
-  }
-  return $geos;
+function dbqueryl($query) {
+	$result = mysql_query($query);
+	return $result;
 }
 
-function dbFetchAll($id,$name)
-{ 
-  $data = array();
-  $title = dbquery("SELECT * from titles left join templates on titles.template=templates.id where titles.id='$id'  ");
-  $result = mysql_fetch_array($title);
-  $templateXML = fopen($result["path"],"r");
-  $contents = stream_get_contents($templateXML);
-  $xml = new SimpleXMLElement($contents);
-  foreach($xml->geo->children() as $geo)
-  {
-    if($geo["name"] == $name)
-    {
-      $data = dbFetch($id,$geo);
-    }
-  }
-  foreach($xml->overlay->children() as $geo)
-  {
-    if($geo["name"] == $name)
-    {
-      $data = dbFetch($id,$geo);
-    }
-  }
-  return $data;
+function listOfGeos($id) {
+	$geos = array();
+	$title = dbquery("SELECT * from titles left join templates on titles.template=templates.id where titles.id='$id'  ");
+	$result = mysql_fetch_array($title);
+	$templateXML = fopen($result["path"], "r");
+	$contents = stream_get_contents($templateXML);
+	$xml = new SimpleXMLElement($contents);
+	foreach ($xml->geo->children() as $geo) {
+		$name = (string) $geo["name"];
+		$geos[$name] = $geo->getName();
+	}
+	foreach ($xml->overlay->children() as $geo) {
+		$name = (string) $geo["name"];
+		$geos[$name] = $geo->getName();
+	}
+	return $geos;
 }
 
-function stripDBFetch($attrs)
-{
-  $result = array();
-  foreach($attrs as $key=>$value)
-    if($key != "x" && $key != "y" && $key != "w" && $key != "h" && $key != "name")
-    {
-      $result[$key] = $value;
-    }
-    return $result;
+function dbFetchAll($id, $name) {
+	$data = array();
+	$title = dbquery("SELECT * from titles left join templates on titles.template=templates.id where titles.id='$id'  ");
+	$result = mysql_fetch_array($title);
+	$templateXML = fopen($result["path"], "r");
+	$contents = stream_get_contents($templateXML);
+	$xml = new SimpleXMLElement($contents);
+	foreach ($xml->geo->children() as $geo) {
+		if ($geo["name"] == $name) {
+			$data = dbFetch($id, $geo);
+		}
+	}
+	foreach ($xml->overlay->children() as $geo) {
+		if ($geo["name"] == $name) {
+			$data = dbFetch($id, $geo);
+		}
+	}
+	return $data;
 }
 
-function dbFetch($id,$xml)
-{ 
-  $data = array();
-  foreach($xml->attributes() as $key=>$value)
-    $data[$key] = (string)$value;
-  $result = dbquery("SELECT * FROM cdb WHERE title_id=\"$id\" AND name=\"".$data["name"]."\";");
-  while($row = mysql_fetch_array($result))
-    $data[$row["key"]] = $row["value"];
-  return $data;
+function stripDBFetch($attrs) {
+	$result = array();
+	foreach ($attrs as $key => $value) {
+		if ($key != "x" && $key != "y" && $key != "w" && $key != "h" && $key != "name") {
+			$result[$key] = $value;
+		}
+	}
+	return $result;
+}
+
+function dbFetch($id, $xml) {
+	$data = array();
+	foreach ($xml->attributes() as $key => $value) {
+		$data[$key] = (string) $value;
+	}
+	$result = dbquery("SELECT * FROM cdb WHERE title_id=\"$id\" AND name=\"" . $data["name"] . "\";");
+	while ($row = mysql_fetch_array($result)) {
+		$data[$row["key"]] = $row["value"];
+	}
+	return $data;
 }
 
 function rgbhex($red, $green, $blue) {
-    $red = 0x10000 * max(0,min(255,$red+0));
-    $green = 0x100 * max(0,min(255,$green+0));
-    $blue = max(0,min(255,$blue+0));
-    return "#".str_pad(strtoupper(dechex($red + $green + $blue)),6,"0",STR_PAD_LEFT);
+	$red = 0x10000 * max(0, min(255, $red + 0));
+	$green = 0x100 * max(0, min(255, $green + 0));
+	$blue = max(0, min(255, $blue + 0));
+	return "#" . str_pad(strtoupper(dechex($red + $green + $blue)), 6, "0", STR_PAD_LEFT);
 }
 
 function mysqlQueryToJsonArray($query) {
 	$result = dbquery($query);
 	$columns = array();
 	$rows = array();
-	while($row = mysql_fetch_assoc($result)) {
+	while ($row = mysql_fetch_assoc($result)) {
 		$columns = array();
 		$dataRow = array();
-		foreach($row as $key => $value) {
+		foreach ($row as $key => $value) {
 			$columns[] = $key;
 			$dataRow[] = $value;
 		}
@@ -106,4 +97,5 @@ function mysqlQueryToJsonArray($query) {
 	$data['columns'] = $columns;
 	return json_encode($data);
 }
+
 ?>
