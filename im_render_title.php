c4bced023137a17ab7cf9a655859c565dcd3e4a8
Reilly Hamilton <reilly.hamilton@espn.com>
2013-03-04 22:33:09 -0800

    Allman -> K&R

--- im_render_title.php
+++ im_render_title.php
@@ -15,74 +15,63 @@ $template_id = $titleRow["template"];
 $result = dbquery("SELECT * from templates where id=\"$template_id\" LIMIT 1;");
 $templateRow = mysql_fetch_array($result);
 
-if($path) {
+if ($path) {
 	$templateRow["path"] = $path;
 }
 
-$templateXML = fopen($templateRow["path"],"r");
+$templateXML = fopen($templateRow["path"], "r");
 $contents = stream_get_contents($templateXML);
 
 $canvas = new Imagick();
-$canvas->newImage(1920,1080,"none","png");
+$canvas->newImage(1920, 1080, "none", "png");
 
 $xml = new SimpleXMLElement($contents);
 
-if($xml->geo->blackBox)
-{
-  foreach($xml->geo->blackBox as $box)
-  {
+if ($xml->geo->blackBox) {
+	foreach ($xml->geo->blackBox as $box) {
 
-    $l = dbFetch($id,$box);
-    blackBox($canvas,$l["x"],$l["y"],$l["w"],$l["h"]);
-  }
+		$l = dbFetch($id, $box);
+		blackBox($canvas, $l["x"], $l["y"], $l["w"], $l["h"]);
+	}
 }
 
-if($xml->geo->slantRectangle)
-{
-  foreach($xml->geo->slantRectangle as $slantRectangle)
-  {
-		$sR = tokenReplace(dbFetch($id,$slantRectangle));
-		slantRectangle($canvas,$sR["x"],$sR["y"],$sR["w"],$sR["h"],$sR["color"]);
-  }
+if ($xml->geo->slantRectangle) {
+	foreach ($xml->geo->slantRectangle as $slantRectangle) {
+		$sR = tokenReplace(dbFetch($id, $slantRectangle));
+		slantRectangle($canvas, $sR["x"], $sR["y"], $sR["w"], $sR["h"], $sR["color"]);
+	}
 }
 
-if($xml->overlay->shadowText)
-{
-  foreach($xml->overlay->shadowText as $text)
-  {
-    $t = tokenReplace(dbFetch($id,$text));
-    shadowedText($canvas,$t["x"],$t["y"],$t["w"],$t["h"],$t["text"],$t["gravity"],$t["font"],$t["color"]);
-  }
+if ($xml->overlay->shadowText) {
+	foreach ($xml->overlay->shadowText as $text) {
+		$t = tokenReplace(dbFetch($id, $text));
+		shadowedText($canvas, $t["x"], $t["y"], $t["w"], $t["h"], $t["text"], $t["gravity"], $t["font"], $t["color"]);
+	}
 }
 
-if($xml->overlay->plainText)
-{
-  foreach($xml->overlay->plainText as $text)
-  {
-    $t = tokenReplace(dbFetch($id,$text));
-    plainText($canvas,$t["x"],$t["y"],$t["w"],$t["h"],$t["text"],$t["gravity"],$t["font"],$t["color"], true);
-  }
+if ($xml->overlay->plainText) {
+	foreach ($xml->overlay->plainText as $text) {
+		$t = tokenReplace(dbFetch($id, $text));
+		plainText($canvas, $t["x"], $t["y"], $t["w"], $t["h"], $t["text"], $t["gravity"], $t["font"], $t["color"], true);
+	}
 }
 
-if($xml->overlay->placeImage)
-{
-  foreach($xml->overlay->placeImage as $image)
-  {
+if ($xml->overlay->placeImage) {
+	foreach ($xml->overlay->placeImage as $image) {
 
-    $l = tokenReplace(dbFetch($id,$image));
-    placeImage($canvas,$l["x"],$l["y"],$l["w"],$l["h"],$l["path"]);
-  }
+		$l = tokenReplace(dbFetch($id, $image));
+		placeImage($canvas, $l["x"], $l["y"], $l["w"], $l["h"], $l["path"]);
+	}
 }
 
 header("Content-Type: image/png");
 echo $canvas;
 
 $thumb = $canvas->clone();
-$thumb->cropImage(1440,1080,0,0);
-$thumb->resizeImage(53,40,Imagick::FILTER_TRIANGLE,1);
+$thumb->cropImage(1440, 1080, 0, 0);
+$thumb->resizeImage(53, 40, Imagick::FILTER_TRIANGLE, 1);
 $thumb->writeImage('thumbs/' . $titleRow["filename"] . '.png');
 
 $canvas->setImageDepth(8);
 $canvas->writeImage('out/' . $titleRow["filename"] . '.png');
-
 ?>
