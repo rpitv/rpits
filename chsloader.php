6fa07eadfffb199fe8a05ed1e3a76a3a86972669
Reilly Hamilton <reilly.hamilton@espn.com>
2013-03-06 22:28:07 -0800

    Let's call that variable 'team' instead of 'teamCode'

--- chsloader.php
+++ chsloader.php
@@ -4,7 +4,7 @@ include ("init.php");
 include ("include.php");
 mysql_select_db("rpihockey");
 
-$teamCode = $_GET["teamCode"];
+$team = $_GET["team"];
 $url = $_GET["url"];
 
 $contents = file_get_contents($system_path_prefix . "chsToJSON.php?url=$url");
@@ -16,13 +16,13 @@ $data = json_decode($contents,true);
 $players = $data['players'];
 
 foreach($players as $p) {
-	$playerResource = dbQuery("SELECT * FROM players WHERE team='$teamCode' AND num='".$p['num']."'");
+	$playerResource = dbQuery("SELECT * FROM players WHERE team='$team' AND num='".$p['num']."'");
 	$row = mysql_fetch_assoc($playerResource);
 	if($row) {
 		if ($p["pos"] != 'G') {
-			dbQuery("UPDATE players SET stype='ho', s1='".$p['gp']."', s2='".$p['g']."', s3='".$p['a']."', s4='".$p['pts']."', s5='".$p['pim']."' WHERE team='$teamCode' AND num='".$p['num']."'");
+			dbQuery("UPDATE players SET stype='ho', s1='".$p['gp']."', s2='".$p['g']."', s3='".$p['a']."', s4='".$p['pts']."', s5='".$p['pim']."' WHERE team='$team' AND num='".$p['num']."'");
 		} else {
-			dbQuery("UPDATE players SET stype='hg', s1='".$p['gp']."', s2='".$p['w']."', s3='".$p['l']."', s4='".$p['t']."', s5='".$p['spct']."', s6='".$p['gaa']."' WHERE team='$teamCode' AND num='".$p['num']."'");
+			dbQuery("UPDATE players SET stype='hg', s1='".$p['gp']."', s2='".$p['w']."', s3='".$p['l']."', s4='".$p['t']."', s5='".$p['spct']."', s6='".$p['gaa']."' WHERE team='$team' AND num='".$p['num']."'");
 		}
 		echo $p['name'] . ', ' . $p['gp'] . ' games played. <br>';
 	}	else {
