72b9bc030f4488b0113dfc6907554b90b5822c6d
Reilly Hamilton <reilly.hamilton@espn.com>
2013-03-06 19:11:26 -0800

    Assume magic quotes is off...

--- sql.php
+++ sql.php
@@ -8,13 +8,13 @@ mysql_select_db($_GET["db"]);
 $firstWord = explode(' ',$_GET["sql"]);
 
 if($firstWord[0] == 'SELECT') {
-	echo mysqlQueryToJsonArray(stripslashes($_GET["sql"]));
+	echo mysqlQueryToJsonArray($_GET["sql"]);
 } else if ($firstWord[0] == 'INSERT') {
-	dbquery(stripslashes($_GET["sql"]));
+	dbquery($_GET["sql"]);
 	$id['id'] = mysql_insert_id();
 	echo json_encode($id);
 } else {
-	dbquery(stripslashes($_GET["sql"]));
+	dbquery($_GET["sql"]);
 }
 
 ?>
