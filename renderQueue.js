9ae2c9e31b6b6e20445ed1b7b52229121a402a05
Ben Vreeland <vreelb@gmail.com>
2013-06-07 22:56:42 -0400

    renderQueue Update

--- renderQueue.js
+++ renderQueue.js
@@ -1,23 +1,30 @@
-function(){
-	var window.renderQueue = new Array();
-	
-	renderQueue.methodName = function addToQueue(){
-		renderQueue.push("<?= $tid ?>"); // Add title id to the queue
-		$('#list').innerHTML = $('#list').innerHTML += '<br/><span tid="<?= $tid ?>"> <?= $tid ?> </span>';
+(function()
+{
+	window.renderQueue = {
+        queue: [],
+    
+        addToQueue: function(tid)
+        {
+    		this.queue.push(tid); // Add title id to the queue
+    		$('#list').append('<br/><span id="'+ tid +'"> '+ tid +' </span>');
+    	},
+    
+    	processQueue: function()
+        {
+    		while ( this.queue.length() > 0 )
+    		{
+    			$.ajax({	// Render something
+    				type: "GET",
+    				url: "im_render_title.php?id="+this.queue[0],
+    				success: function(data) {
+    					this.renderQueue.shift(); // Remove first element from queue (it is now done)
+    					document.getElementById(this.queue[0]).bgColor="#00FF00"; // Mark as green on list			
+    				}
+    			});
+    		}
+    	}
 	};
-
-	renderQueue.function processQueue(){
-		while ( renderQueue.length() > 0 )
-		{
-			$.ajax({	// Render something
-				type: "GET",
-				url: "im_render_title.php?id="+renderQueue[0],
-				success: function(data) {
-					renderQueue.shift(); // Remove first element from queue (it is now done)
-					document.getElementById(renderQueue[0]).bgColor="#00FF00"; // Mark as green on list			
-				}
-			});
-		}
-	};
+    
+    
 	
 }());
