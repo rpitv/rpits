2aca185ac74b7d205ec62818a8182810af677699
Reilly Hamilton <reilly.hamilton@espn.com>
2013-11-06 23:10:41 -0800

    Add support for linear gradients to be used as colors for slantRectangles
    use the following syntax: linear-gradient(to [direction], #color stop%, rgb(r,g,b) stop%)
    
    For the most part, this should follow the CSS3 spec; note the following: first and last stop% can be ommitted. [to direction] can be ommitted (defaults to 'to bottom'). Color will accept HTML colors, Imagick colors, hex colors, and rgb() colors. Stop distances must be in %s (would pixels be useful??).

--- im_test.php
+++ im_test.php
@@ -1,41 +1,28 @@
-<pre>
-
 <?php
 
 // This file is just for checking that functions / ideas work independently
+//
+// It's currently a test case for fillRectangle
 
 
-//include("include.php");
+include("include.php");
 //include("imagick_include.php");
 
+$gradient = $_GET['gradient'];
+$w = 600;
+$h = 300;
 
+$canvas = new Imagick();
+$canvas->newImage($w, $h, "none", 'png');
+$canvas->setImageDepth(8);
+$canvas->setimagecolorspace(imagick::COLORSPACE_SRGB);
 
-//$result = stripDBFetch(dbFetchAll(5,"AwayTeamName"));
-//  print_r($result);
+$result = fillRectangle($w,$h,$gradient);
+$canvas->compositeImage($result,Imagick::COMPOSITE_OVER,0,0);
 
-$page = fopen("tabular_data.txt","r");
-$contents = stream_get_contents($page);
+header("Content-Type: image/" . IMGFMT);
+echo $canvas;
 
-$result = parser($contents);
-
-foreach($result as $img)
-{
-  echo "<img src=\"http://rpiathletics.com/" . $img . "\" />$img<br>";
-}
-
-function parser($data)
-{
-  preg_match_all("/image_path=(.*jpg)/i",$data,$data2);
-  foreach($data2[1] as $line)
-  {
-    $num = preg_match_all("/\/\/([A-Za-z]*)_/",$line,$vals);
-    print_r($vals);
-  }
-  return $data2[1];
-}
 
 ?>
-
-/common/controls/image_handler.aspx?image_path=/images/2012/8/30//KyleMcGovern_23.jpg&thumb_prefix=rp_roster
-
 
