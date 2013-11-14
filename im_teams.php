f8e0dfd0cae86ce0ac2ceef3981e49681db2cde1
Reilly Hamilton <reilly.hamilton@espn.com>
2013-11-13 18:40:49 -0800

    Include init in order to pull database name for editableTables

--- im_teams.php
+++ im_teams.php
@@ -1,3 +1,5 @@
+<?php include("init.php"); ?>
+
 <script src="js/lib/jquery-1.5.1.min.js" type="text/javascript"></script>
 <script src="js/lib/jquery-ui-1.8.12.custom.min.js" type="text/javascript"></script>
 <script src="im.js" type="text/javascript"></script>
