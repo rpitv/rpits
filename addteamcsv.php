5508caf2181118aa88dc764ddf9776851f9dd6b3
Ben Vreeland <vreelb@rpi.edu>
2014-02-13 02:26:33 -0500

    Added Table Parsing for CHS Rosters
    Plans are in the works to clean up this code and make ready for new changes for make benefit future of RPITS.
    Parsing from CHS links, etc.

--- addteamcsv.php
+++ addteamcsv.php
@@ -1,6 +1,7 @@
 <title>Team Roster Adder (via CSV)</title>
 <?
 include ("init.php");
+include ("include.php");
 $team_sel = $_POST["team_sel"];
 $csv = $_POST["csv"];
 
@@ -22,21 +23,33 @@ else
 ?>
 
 <h1>Add Team Roster via CSV</h1>
+
+<label>Pull from CollegeHockeyStats URL: (not working yet)
+  <input type="text" name="pull_url" size="100" />
+  <button name="pull" onclick="alert(pull_url.value)">Pull Roster</button>
+</label>
+<br/>
+<label>Parse HTML Table:<br/>
+  <div id="rosterTable" style="visibility: auto;"></div>
+  <div id="tableEntry">
+    <textarea id="tableHTML" rows="10" cols="100"></textarea>
+    <button id="parseButton" onclick="parse_table_HTML($('#tableHTML').val());">Parse Roster</button>
+  </div>
+  <button id="showTableEntry" onclick="$('#tableEntry').toggle()">Toggle Table Entry Form</button>
+</label>
+
+<br/>
 <form action="addteamcsv.php" method="POST">
   <label>Team Name:
     <input type="text" name="team_sel" size="10" /> (Form: organization-team)
   </label>
-  <br/>
-  <label>Pull from CollegeHockeyStats URL: (not working yet)
-    <input type="text" name="pull_url" size="100" />
-    <button name="pull" onclick="alert(pull_url.value)">Parse Roster</button>
-  </label>
-  <br/>
   <p>Entries should be in the form: num|first|last|pos|height|weight|year|hometown|stype|s1|s2|s3|s4|s5|s6|s7|s8<br/>
   Missing information must be delimited (e.g., no weight -> ...height||year...)<br/>
   Missing information or stats at the end of the line can be ignored.</p>
-  <textarea name="csv" rows="30" cols="100"></textarea>
+  <textarea id="csv_textarea" name="csv" rows="30" cols="100"></textarea>
   <input type="submit" name="Submit" />
 </form>
+<script src="./js/lib/jquery-1.8.3.js" type="text/javascript"></script>
+<script src="./parse_roster.js"></script>
 <? } ?>
 
---
+++ parse_roster.js
@@ -0,0 +1,103 @@
+// Pass in the table DOM Object, and the number of initial rows not containing data
+function parse_table_object(tableObject, rowsToSkip) {
+  if (rowsToSkip === undefined) {
+    rowsToSkip = 1; // assume 1 header row
+  }
+
+  //var second = string.charAt(1);
+  if (second === second.toLowerCase() && second !== second.toUpperCase())
+  {
+    // second character is a lowercase letter
+  }
+
+}
+
+// Pass in the table HTML, and the number of initial rows not containing data
+function parse_table_HTML(table_HTML, rowsToSkip) {
+  if (rowsToSkip === undefined) {
+    rowsToSkip = 1; // assume 1 header row
+  }
+
+  // Sanitize nbsp weirdness
+  table_HTML = table_HTML.replace(/\&nbsp\;/g, ' ' );
+
+  $('#rosterTable').html(table_HTML);
+  $('#rosterTable table').css('font-size', '8pt');
+  $('#tableEntry').hide();
+  $('#showTableEntry').show();
+
+  var num_players = 0
+  var submission_string = '';
+  var temp1 = '';
+  var temp2 = '';
+  var temp_stats = [[],[],[],[],[],[],[],[]];
+
+  $('tr').slice(rowsToSkip).each(function(){
+    num_players++;
+
+    $(this).find('td').each(function(index){
+      switch (index){
+      case 0: // parse number
+        temp_stats[0] = $(this).text().trim().replace(/\#/g, '') + '|';
+        break;
+      case 1: // parse FIRST and LAST name and DRAFT
+        temp1 = $(this).text().trim().replace("\'", "\\\'");
+        temp2 = temp1.split(' ');
+        
+        if (temp2[temp2.length-1].indexOf('(') >= 0) {  // get (DRAFT) out
+          temp2.pop();
+        }
+
+        temp_stats[1] = temp2.shift() + '|';  // get FIRST name
+        
+        var last_name = '';
+        if (temp2[0]){
+          temp2.forEach(function(elem){ // get all words in LAST name
+            last_name += elem + ' ';
+          });
+          temp_stats[2] = last_name.trim() + '|';
+        }
+        break;
+      case 2: // parse year
+        temp1 = $(this).text().trim();
+        temp2 = temp1.charAt(1).toLowerCase(); 
+        temp_stats[6] = temp1.charAt(0) + temp2 + '|';
+        break;
+      case 3: // parse position
+        temp_stats[3]= $(this).text().trim() + '|';
+        break;
+      case 4: // parse height
+        temp_stats[4] = $(this).text().trim() + '|';
+        break;
+      case 5: // parse weight
+        temp_stats[5] = $(this).text().trim() + '|';
+        break;
+      case 6: // parse handedness
+        
+        break;
+      case 7: // parse age
+        //submission_string += $(this).text().trim() + '|';
+        break;
+      case 8: // parse hometown and prev team
+        temp1 = $(this).text().trim();
+        temp2 = temp1.split(' / ');
+        temp_stats[7] = temp2[0];
+        break;
+      }
+    });
+    
+    temp_stats.forEach(function(elem){ // get all words in LAST name
+      submission_string += elem;
+    });
+    submission_string += '\n';
+    
+  });
+
+  $('#csv_textarea').val(submission_string.trim());
+
+}
+
+// Pass in an entire page of HTML
+function parse_page_for_table(pageHTML) {
+  
+}
