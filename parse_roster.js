314e09997b92cb23b1d922fa82fb5f2e72e78cf1
Ben Vreeland <vreelb@rpi.edu>
2014-03-11 23:18:04 -0400

    Store Draft Pick Information when scraping CHS
    Also updates how it collects the info to not be retarded.

--- parse_roster.js
+++ parse_roster.js
@@ -17,28 +17,28 @@ function parse_table_HTML(table_HTML, rowsToSkip) {
   var submission_string = '';
   var temp1 = '';
   var temp2 = '';
-  var temp_stats = [[],[],[],[],[],[],[],[]];
 
   $('#rosterTable tr').slice(rowsToSkip).each(function(){
+    var player = new Object();
     num_players++;
     num_rows = $(this).find('td').length;
 
     $(this).find('td').each(function(index){
       switch (index){
       case 0: // parse number
-        temp_stats[0] = $(this).text().trim().replace(/\#/g, '') + '|';
+        player.number = $(this).text().trim().replace(/\#/g, '') + '|';
         break;
       case 1: // parse FIRST and LAST name and DRAFT
         temp1 = $(this).text().trim().replace("\'", "\\\'");
 
         temp2 = temp1.split('|');
-        temp_stats[1] = temp2.shift() + '|';  // get FIRST name(s)
+        player.first_name = temp2.shift() + '|';  // get FIRST name(s)
 
         temp2 = temp2[0].split(' ');
 
         if (temp2[temp2.length-1].indexOf('(') >= 0) {  // get (DRAFT) out
           temp1 = temp2.pop();
-          temp1 = temp1.substr(1,3);
+          player.draft_pick = temp1.substr(1,3);
         }
         
         var last_name = '';
@@ -46,47 +46,50 @@ function parse_table_HTML(table_HTML, rowsToSkip) {
           temp2.forEach(function(elem){ // get all words in LAST name
             last_name += elem + ' ';
           });
-          temp_stats[2] = last_name.trim() + '|';
+          player.last_name = last_name.trim() + '|';
         }
         break;
       case 2: // parse year
         temp1 = $(this).text();
         temp2 = temp1.charAt(1).toLowerCase(); 
-        temp_stats[6] = temp1.charAt(0) + temp2 + '|';
+        player.year = temp1.charAt(0) + temp2 + '|';
         break;
       case 3: // parse position
-        temp_stats[3]= $(this).text().trim() + '|';
+        player.position = $(this).text().trim() + '|';
         break;
       case 4: // parse height
-        temp_stats[4] = $(this).text().trim() + '|';
+        player.height = $(this).text().trim() + '|';
         break;
       case 5: // parse weight (M)
         if (num_rows == 9) {
-            temp_stats[5] = $(this).text().trim() + '|';
+            player.weight = $(this).text().trim() + '|';
           } else { //handedness (W)
-            //temp_stats[5] = $(this).text().trim() + '|';
+            //player.hand = $(this).text().trim() + '|';
           }
         break;
       case 6: // parse handedness (M), age (W)
         
         break;
       case 7: // parse age (M), hometown + etc. (W)
-        //submission_string += $(this).text().trim() + '|';
+        //player.age += $(this).text().trim() + '|';
         if (num_rows == 8) {
-          temp_stats[7] = $(this).text().trim().split(' / ')[0];
+          player.hometown = $(this).text().trim().split(' / ')[0];
         }
         break;
       case 8: // parse hometown and prev team (M)
         temp1 = $(this).text().trim().split(' / ');
         temp2 = temp1[0].split(', ');
-        temp_stats[7] = (temp2[0]+ ", " + getAbbr(temp2[1])).replace("\'", "\\\'");
+        player.hometown = (temp2[0]+ ", " + getAbbr(temp2[1])).replace("\'", "\\\'");
         break;
       }
     });
     
-    temp_stats.forEach(function(elem){ // get all words in LAST name
-      submission_string += elem;
-    });
+    submission_string += player.number + player.first_name + player.last_name + player.position + player.height + player.weight + player.year + player.hometown;
+
+    if (player.draft_pick) {
+      submission_string += '||||||||' + player.draft_pick;
+    }
+
     submission_string += '\n';
     
   });
