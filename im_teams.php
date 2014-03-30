3e3ad9721f1826d2aef75f741ff5c8ecdf3915d4
Ben Vreeland <vreelb@rpi.edu>
2014-03-30 12:28:11 -0400

    Updates all (MHockey) team stats (via CHN)
    Resolves #62.

--- im_teams.php
+++ im_teams.php
@@ -1,6 +1,6 @@
 <?php include("init.php"); ?>
 
-<script src="js/lib/jquery-1.5.1.min.js" type="text/javascript"></script>
+<script src="js/lib/jquery-1.8.3.js" type="text/javascript"></script>
 <script src="js/lib/jquery-ui-1.8.12.custom.min.js" type="text/javascript"></script>
 <script src="im.js" type="text/javascript"></script>
 <script type="text/javascript">
@@ -36,23 +36,61 @@ $(function() {
 
 });
 
+$(document).ready( function(){
+
+  $("#UpdateAllCHN").click( function(){
+    $("#details").empty(); // get rid of old update info (if any)
+    $(".erow").each( function(){
+      var chn_id = $(this).find(".chn_id a").text();
+      if ($.isNumeric(chn_id)) {
+        var team_name = $(this).find(".player_abbrev").text();
+        $("<div>").load("statsloader.php?tid="+chn_id, function(){
+          $("#details").append( '<div><h3><a href="im_peditor.php?team_sel='+team_name+'">'+team_name+'</a></h3>'+$(this).html()+'</div>' );
+        });
+      }
+    }).promise().done(function(){
+      if ($("#header span").size()==0) { // only show stats message if not there
+        $("#header").append('<span>Stats Updated, details <a href="#details">below the table</a>.</span>');
+      }
+    });
+  });
+});
 
 </script>
 <style type="text/css">
-	tr {
+  #teamsList table {
+    margin-top: 1em;
+  }
+
+	#teamsList tr {
 		height:30px;
 	}
-	tr td {
+	#teamsList tr td {
 		width:100px;
 	}
-	tr.editing input, tr.nrow input {
+	#teamsList tr.editing input, tr.nrow input {
 		width:100px;
 	}
-	th {
+	#teamsList th {
 		text-align: left;
 	}
-	tr.erow td.action {
+	t#teamsList r.erow td.action {
 		width:150px;
 	}
+
+  #details div {
+    min-width: 300px;
+    min-height: 850px;
+    float: left;
+    clear: right;
+  }
+  #details h3 {
+    margin-bottom: .2em;
+  }
 </style>
+<div id="header">
+  <h1>Teams</h1>
+  <button type="button" name="UpdateAllCHN" id="UpdateAllCHN">Update Men's Hockey Stats (CHN)</button>
+</div>
 <div id="teamsList"></div>
+<div id="details"></div>
