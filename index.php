5fb3735615762a0a255c2befd55b45b0d8d0cd65
Ben Vreeland <vreelb@rpi.edu>
2014-05-29 20:45:37 -0400

    Updated wording on the index page.

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ include('init.php');
 <p style="font-family:'Arial',sans-serif;font-size:12px;font-weight:bold"><img src="rpits.jpg" style="vertical-align:-1px;"/> is proud to be a part of <img src="rpigs.jpg" style="vertical-align:-1px" /></p>
 <h2>List of Current Pages</h2>
 <ul>
-	<li><a href="im_ui.php">Titles UI (ImageMagick based)</a> - Send titles to keyer, edit general titles. Also edit events and their participants</li>
-	<li><a href="im_event_title.php">Event / Title List editor</a> - Customize the list of titles to be displayed in the UI</li>
-	<li><a href="im_peditor.php">Player/Roster Editor</a> - Edit individual players' details and stats, control which players are on which teams</li>
+	<li><a href="im_ui.php">Titles UI (ImageMagick based)</a> - Main user interface for editing titles and sending them to the keyer.</li>
+	<li><a href="im_event_title.php">Event / Title List editor</a> - Customize the list of titles to be displayed in the UI.</li>
+	<li><a href="im_peditor.php">Player/Roster Editor</a> - Edit individual players' details/stats, control which players are on which teams</li>
 	<li><a href="im_teams.php">Teams editor</a> and <a href="im_organizations.php">Organizations editor</a> - Edit teams and their respective organizations.</li>
-	<li><a href="addteamcsv.php">Add a Roster</a> - Adds a players and their stats to an empty team via CSV</li>
-	<li><a href="stypeEditor.php">Stat Type label editor</a> - Edit labels associated with stats</li>
-	<li><a href="genall.php">Player Title Generator</a> - Generate the player titles for an entire team (or two), clear the cache</li>
-	<li><a href="bugSelector.php">Bug Selector</a> - Select 1920x1080 Portable Network Graphic (PNG) for use as a broadcast bug</li>
+	<li><a href="addteamcsv.php">Add a Roster</a> - Add players and their stats to a team.</li>
+	<li><a href="stypeEditor.php">Stat Type label editor</a> - Edit labels associated with stats.</li>
+	<li><a href="genall.php">Player Title Generator</a> - Generate the player titles for an entire team (or two), clear the cache.</li>
+	<li><a href="bugSelector.php">Bug Selector</a> - Select a 1920x1080 .PNG file for use as a broadcast bug.</li>
 </ul>
 <h3>Deprecated Pages</h3>
 <ul>
