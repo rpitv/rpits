720baceeca8d27b5fd506c54dfab07e47bf81a95
Reilly Hamilton <reilly.hamilton@espn.com>
2014-07-04 21:37:50 -0700

    Modify config.sample.php to point to default/recommended install location

--- config.sample.php
+++ config.sample.php
@@ -5,8 +5,10 @@ $mysql_server_username = "root";
 $mysql_server_password = "";
 $mysql_database_name = "rpits";
 
-$keyer_url = "http://192.168.1.23:4567/"; // trailing slashes are important
-$system_path_prefix = "http://127.0.0.1/hockey/";
+$system_path_prefix = "http://127.0.0.1/rpits/";
+
+// Only necessary in order to communicate with an Exavideo keyer, can be ignored
+$keyer_url = "http://127.0.0.1:4567/"; // trailing slashes are important
 
 define('IMGFMT','png'); // 'tga' also supported
 
