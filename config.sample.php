202f7421bd3d71a546a9bafd9e578e1ec077f1be
Andrew Armenia <andrew@asquaredlabs.com>
2020-01-28 09:31:00 -0500

    db_mysqli.php: added global declarations to make it work
    also added usage to config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -4,6 +4,11 @@ $mysql_server_address = "127.0.0.1";
 $mysql_server_username = "root";
 $mysql_server_password = "";
 $mysql_database_name = "rpits";
+include_once("db_mysqli.php");
+rpits_mysql_connect(
+    $mysql_server_address, $mysql_server_username,
+    $mysql_server_password, $mysql_database_name
+);
 
 $system_path_prefix = "http://127.0.0.1/rpits/";
 
--- db_mysqli.php
+++ db_mysqli.php
@@ -1,6 +1,12 @@
 <?php
 
 function rpits_mysql_connect($server, $user, $password, $database) {
+    global $rpits_db_connection;
+
+    if (isset($rpits_db_connection)) {
+        trigger_error("rpits_mysql_connect was called twice.", E_WARNING);
+    }
+
     $rpits_db_connection = new mysqli($server, $user, $password, $database);
 
     if ($mysqli->connect_error) {
@@ -11,39 +17,47 @@ function rpits_mysql_connect($server, $user, $password, $database) {
 }
 
 function _rpits_db_check() {
+    global $rpits_db_connection;
     if (!isset($rpits_db_connection)) {
         die('no database connection');
     }
 }
 
 function rpits_db_errno() {
+    global $rpits_db_connection;
     _rpits_db_check();
     return $rpits_db_connection->errno;
 }
 
 function rpits_db_error() {
+    global $rpits_db_connection;
     _rpits_db_check();
     return $rpits_db_connection->error;
 }
 
 function rpits_db_fetch_array($result) {
+    global $rpits_db_connection;
     return $result->fetch_array();
 }
 
 function rpits_db_fetch_assoc($result) {
+    global $rpits_db_connection;
     return $result->fetch_assoc();
 }
 
 function rpits_db_insert_id() {
+    global $rpits_db_connection;
     _rpits_db_check();
     return $rpits_db_connection->insert_id;
 }
 
 function rpits_db_num_rows($result) {
+    global $rpits_db_connection;
     return $result->num_rows();
 }
 
 function rpits_db_query($query_string) {
+    global $rpits_db_connection;
     return $rpits_db_connection->query($query_string);
 }
 
